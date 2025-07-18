3ce2057b9c46072375fe36cdbc9a777c16c7c481
Filip Lysak <filiplysak@Filips-MacBook-Air.local>
2025-07-18 14:18:00 +0200

    working model for transcription, and additional tests passed for video

---
+++ .pio/build/seeed_xiao_esp32s3/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/seeed_xiao_esp32s3/FrameworkArduino/cbuf.cpp.o: \
+ /Users/filiplysak/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ /Users/filiplysak/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- src/utils/loggers.cpp
+++
@@ -1,2 +0,0 @@
-#include "utils/logger.h"
-// Empty .cpp so the linker is happy; all logic is inline in the header.
