68e8daec0cca17a8e90ee3ca4d74cf71dd2dc389
Samudra-Gargo <samudra6289@gmail.com>
2025-07-23 07:43:13 -0500

    Add saving audio to google cloud(Since paid, I didnt gve any bucket name, Flomad Labs can add the bucket and the json file, and it shoud run, also saves audio transcriptions to vector database, for better search results(couldnt test)

--- .pio/build/seeed_xiao_esp32s3/FrameworkArduino/cbuf.cpp.d
+++ .pio/build/seeed_xiao_esp32s3/FrameworkArduino/cbuf.cpp.d
@@ -1,3 +1,3 @@
 .pio/build/seeed_xiao_esp32s3/FrameworkArduino/cbuf.cpp.o: \
- /Users/filiplysak/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /Users/filiplysak/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
+ C:/Users/pirat/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/pirat/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
