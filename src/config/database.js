a15ebe1e4a5a1634d2644c558b4170a7924e5147
Raheb Saeed <raheebareef@gmail.com>
2025-10-08 14:35:30 +0100

    fix database credentials from .env file

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,23 @@
-const { Pool } = require('pg');
+const { Sequelize } = require('sequelize');
+const config = require('./index'); // Imports the config from index.js
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'file_uploads', // or 'postgres' if you're using that
-  password: '', // leave it blank if no password is set
-  port: 5432,
+// Add this line for debugging to be 100% sure
+console.log('Connecting with URL:', config.databaseUrl); 
+
+// Create a new Sequelize instance
+const sequelize = new Sequelize(config.databaseUrl, {
+  dialect: 'postgres',
+  logging: false,
 });
 
-module.exports = pool;
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('PostgreSQL connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the PostgreSQL database:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = { sequelize, connectDB };
