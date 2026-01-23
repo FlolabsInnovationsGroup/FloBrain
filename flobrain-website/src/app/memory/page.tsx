2b1086cf9854b2e471c2d54d2b500af055c3f785
indraAraujo <indra.araujo.santos@gmail.com>
2026-01-23 17:03:16 -0300

    creates memory page with graph visualization

--- flobrain-website/src/app/memory/page.tsx
+++ flobrain-website/src/app/memory/page.tsx
@@ -1,8 +1,23 @@
+"use client"
+import { useState } from "react";
+import { MemoryGraph } from "./components/memory-graph/";
+import { MemoryNodeDetailsDialog } from "./components/memory-node-details-dialog";
+import { memoryNode } from "../../types/MemoryNodes";
+
 export default function Memory() {
+  const [selectedNode, setSelectedNode] = useState<memoryNode | null>(null);
+  const [openMemoryNodeDialog, setOpenMemoryNodeDialog] = useState<boolean>(false);
+
+  function onOpenMemoryNodeDialog (node: memoryNode) {
+      setSelectedNode(node);
+      setOpenMemoryNodeDialog(true);
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 ">
       <h1 className="text-4xl font-bold text-[var(--color-memory)]">Universal Memory</h1>
-      <p className="mt-4 text-zinc-400">Retrieving data blocks...</p>
+      <MemoryGraph onOpenMemoryNodeDialog={onOpenMemoryNodeDialog}/>
+      <MemoryNodeDetailsDialog open={openMemoryNodeDialog} setOpen={setOpenMemoryNodeDialog} description={selectedNode?.name}/>
     </main>
   );
 }
+
