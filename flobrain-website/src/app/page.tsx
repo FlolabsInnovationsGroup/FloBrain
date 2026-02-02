e03cf948cd6770500f0fddd2be862c41b0bae1ee
Rediet <redietbekele02@outlook.com>
2026-02-02 21:53:38 +0300

    feat: finalize root page logic and landing page redirection

--- flobrain-website/src/app/page.tsx
+++ flobrain-website/src/app/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation';
 import HomePage from "./(app)/home/index"; 
 
 export default function Home() {
-  const userLogged = true; 
+  const userLogged = false; 
 
   if (userLogged) {
     redirect('/home'); 
