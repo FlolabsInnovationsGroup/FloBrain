946221a1bbd85bd46573e747bd45771b50550ba9
Arsema Mandefro <arsemamandefrozegeye@gmail.com>
2026-03-25 20:58:51 +0300

    updated email settings

--- flobrain-core/backend/flobrain/settings.py
+++ flobrain-core/backend/flobrain/settings.py
@@ -146,12 +146,12 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.JSONRenderer",
     ],
 }
-
-# Email (SMTP via Gmail, port 587 TLS)
+# Email (SMTP)
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.environ.get("SMTP_HOST", "smtp.gmail.com")
-EMAIL_PORT = int(os.environ.get("SMTP_PORT", 587))
-EMAIL_USE_TLS = True
+EMAIL_HOST = os.environ.get("SMTP_HOST", "")
+EMAIL_PORT = int(os.environ.get("SMTP_PORT", 465))
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.environ.get("SMTP_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("SMTP_PASS", "")
 SALES_EMAIL = os.environ.get("SALES_EMAIL", "")
