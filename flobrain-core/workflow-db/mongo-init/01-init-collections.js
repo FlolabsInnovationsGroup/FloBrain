6fbfe28cfd59baecba72928f0b3e808474cb605b
Tanmay Jagtap <tanmayjagtap@Tanmays-MacBook-Air.local>
2026-04-27 23:58:43 -0500

    Add Dockerized workflow database setup

---
+++ flobrain-core/workflow-db/mongo-init/01-init-collections.js
@@ -0,0 +1,20 @@
+db = db.getSiblingDB('flobrain');
+
+const collections = [
+    'workflows',
+    'workflow_steps',
+    'llmcalls',
+    'messages',
+    'locations',
+    'presetworkflows_planA',
+    'presetworkflowsteps',
+    'agents',
+    'tools'
+];
+
+collections.forEach((c) => {
+    const exists = db.getCollectionNames().includes(c);
+    if (!exists) {
+      db.createCollection(c);
+    }
+  });
